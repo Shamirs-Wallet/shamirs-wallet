f1657fc1002766bb83bb1516318cffd1bc5f0613
Mathias Schütz <schuetz@optimal-systems.de>
2020-03-13 18:39:41 +0100

    implemented select security page logic

--- src/app/pages/select-security/select-security.page.ts
+++ src/app/pages/select-security/select-security.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-select-security',
@@ -6,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-security.page.scss'],
 })
 export class SelectSecurityPage implements OnInit {
+  routeSubscription: Subscription;
+  readMode: boolean;
+  wordLength: number;
+  mask: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe(async params => {
+      this.readMode = params.read === 1;
+      this.wordLength = Number(params.wordLength);
+    });
+
+    this.mask = new FormGroup({
+      combination: new FormControl({ value: 35, required: true })
+    });
   }
 
   getWordsRouterLink() {
-    return ['/words', 0, 24, 35]; // TODO Read oder Write?; Wörterlänge holen; Sicherheitskombination holen
+    return ['/words', this.readMode ? 1 : 0, this.wordLength, this.mask.controls.combination.value];
   }
 }
