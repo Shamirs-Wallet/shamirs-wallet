1848fb27fd93abbeb1707a618cf99329c2072527
Mathias Schütz <schuetz.mathias@outlook.com>
2020-03-13 18:15:47 +0100

    implemented word length selection logic

--- src/app/pages/select-word-length/select-word-length.page.ts
+++ src/app/pages/select-word-length/select-word-length.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-select-word-length',
@@ -9,16 +11,23 @@ import { Subscription } from 'rxjs';
 export class SelectWordLengthPage implements OnInit {
   routeSubscription: Subscription;
   readMode: boolean;
+  mask: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.routeSubscription = this.route.params.subscribe(async params => {
-      
+      this.readMode = params.read === 1;
+    });
+
+    this.mask = new FormGroup({
+      wordLength: new FormControl({ value: 24, required: true })
     });
   }
 
   getSelectSecurityRouterLink() {
-    return ['/select-security', 0, 24]; // TODO Read oder Write?; Wörterlänge holen
+    return ['/select-security', this.readMode ? 1 : 0, this.mask.controls.wordLength.value];
   }
 }
