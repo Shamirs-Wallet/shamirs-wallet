74c6da2d14cf92f46185edb3faddc823e948f85e
Mathias Schütz <schuetz@optimal-systems.de>
2020-04-14 12:42:43 +0200

    added content to finish page

--- src/app/pages/finish/finish.page.ts
+++ src/app/pages/finish/finish.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-finish',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinishPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  backToHome() {
+    this.router.navigate(['/home']); // Navigate Back to Home
   }
 
 }
