9a27a1b2170952f24aa13d179e7bb9be0493587c
Mathias Schütz <schuetz@optimal-systems.de>
2020-05-13 17:13:39 +0200

    added translation

--- src/app/pages/combination/combination.module.ts
+++ src/app/pages/combination/combination.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { CombinationPageRoutingModule } from './combination-routing.module';
-
 import { CombinationPage } from './combination.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CombinationPageRoutingModule
+    CombinationPageRoutingModule,
+    TranslateModule.forChild()
   ],
   declarations: [CombinationPage]
 })
