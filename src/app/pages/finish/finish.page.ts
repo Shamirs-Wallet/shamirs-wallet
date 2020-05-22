2ab6a91841b9980ccf8e05a5d54dc65770097653
Mathias Schütz <schuetz.mathias@outlook.com>
2020-05-22 17:06:41 +0200

    added animations

--- src/app/pages/finish/finish.page.ts
+++ src/app/pages/finish/finish.page.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ShamirService } from 'src/app/services/shamir.service';
+import { fadeIn } from 'src/app/animations/animations';
 
 @Component({
   selector: 'app-finish',
   templateUrl: './finish.page.html',
   styleUrls: ['./finish.page.scss'],
+  animations: [
+    fadeIn
+  ]
 })
 export class FinishPage {
   password = this.shamir.getSuperPasswort();
