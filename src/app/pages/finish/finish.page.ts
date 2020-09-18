3dc1f29b3ec08f579449a6d5fefa4d19344b48ed
Mathias Schütz <schuetz@optimal-systems.de>
2020-09-18 17:24:23 +0200

    added asking for rating toast

--- src/app/pages/finish/finish.page.ts
+++ src/app/pages/finish/finish.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, ToastController, Platform } from '@ionic/angular';
 import { ShamirService } from 'src/app/services/shamir.service';
 import { fadeIn } from 'src/app/animations/animations';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-finish',
@@ -11,15 +12,46 @@ import { fadeIn } from 'src/app/animations/animations';
     fadeIn
   ]
 })
-export class FinishPage {
+export class FinishPage implements OnInit {
   password = this.shamir.getSuperPasswort();
 
   constructor(
     private navigation: NavController,
-    public shamir: ShamirService
+    public shamir: ShamirService,
+    private toastController: ToastController,
+    private platform: Platform,
+    private translate: TranslateService
   ) { }
 
   backToHome() {
     this.navigation.navigateRoot(['/home']); // Navigate Back to Home
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => this.openRateAppToast(), 4000);
+  }
+
+  async openRateAppToast() {
+    const toast = await this.toastController.create({
+      message: await this.translate.get('pages.finish.rating.message').toPromise(),
+      buttons: [
+        {
+          side: 'end',
+          text: await this.translate.get('pages.finish.rating.rate-button').toPromise(),
+          handler: () => {
+            if (this.platform.is('ios')) {
+              // TODO
+            } else if (this.platform.is('android')) {
+              window.open('https://play.google.com/store/apps/details?id=ms.shamirswallet.app');
+            }
+          }
+        }, {
+          text: 'X',
+          role: 'cancel',
+          handler: () => { }
+        }
+      ]
+    });
+    toast.present();
   }
 }
